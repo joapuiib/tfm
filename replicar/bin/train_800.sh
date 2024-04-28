46304f098896d40207fd5da50bda3e7bd2ed65cb
Joan Puigcerver <joapuiib@gmail.com>
2024-04-28 13:22:02 +0200

    replicar: more experiment scripts

--- replicar/bin/train_800.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-model_dir="$HOME/tfm/models/type-800"
-replicar="$HOME/tfm/replicar/train.py type-800 --target type --size 800 --batch_size 24 --n_workers 8"
-datetime=$(date +%Y%m%d-%H%M%S)
-
-($replicar | tee $model_dir/${datetime}_stdout.log) 3>&1 1>&2 2>&3 | tee $model_dir/${datetime}_stderr.log
-
---
+++ replicar/bin/train_toplabel-800.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+model_dir="$HOME/tfm/replicar/models/top_label-800"
+replicar="$HOME/tfm/replicar/train.py top_label-800 --target top_label --size 800 --batch_size 20 --n_workers 8 --model_path $model_dir"
+datetime=$(date +%Y%m%d-%H%M%S)
+
+($replicar | tee $model_dir/${datetime}_stdout.log) 3>&1 1>&2 2>&3 | tee $model_dir/${datetime}_stderr.log
+
