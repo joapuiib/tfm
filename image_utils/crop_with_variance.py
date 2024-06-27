a4651479232ebfdae42bdfe2ed50475424a3b123
Joan Puigcerver <joapuiib@gmail.com>
2024-06-27 13:33:09 +0200

    crop n resize

--- image_utils/crop_with_variance.py
+++ image_utils/crop_with_variance.py
@@ -12,22 +12,32 @@ Image.MAX_IMAGE_PIXELS = 260000000
 
 class CropWithVariance:
     def __init__(self, images_csv, dest_dir, crop_size, shift):
+        self.images_csv = images_csv
         self.origin_dir = os.path.dirname(images_csv)
         self.dest_dir = dest_dir
         self.crop_size = crop_size
         self.shift = shift
 
-        self.result_df = None
+        self.result = []
 
-    def crop_and_discard(self, csv_images):
-        self.load_df(csv_images)
+    def crop_and_discard(self):
+        self.load_df(self.images_csv)
         self.process_images()
+        self.save_result_df()
 
 
     def load_df(self, csv_images):
         print(f'Loading dataframe from {csv_images}')
         self.df_images = pd.read_csv(csv_images)
         print(f'Loaded {len(self.df_images)} images')
+
+
+    def save_result_df(self):
+        name = os.path.basename(self.images_csv)
+        result_csv = os.path.join(self.dest_dir, name)
+        result_df = pd.DataFrame(self.result)
+        result_df.to_csv(result_csv, index=False)
+        print(f'Saved {len(result_df)} crops to {result_csv}')
 
 
     def process_images(self):
@@ -39,24 +49,29 @@ class CropWithVariance:
 
             exists = self.check_image_exists(filename)
             if exists:
-                self.create_crops(filename)
+                self.create_crops(entry, filename)
 
 
     def check_image_exists(self, filename):
         return os.path.exists(filename)
 
 
-    def create_crops(self, filename):
+    def create_crops(self, entry, filename):
         img = Image.open(filename)
         crop_regions = self.compute_crop_regions(img)
         print(f'Computed {len(crop_regions)} crops for {filename}')
 
         for crop_region in crop_regions:
-            crop_path = self.get_crop_path(filename, crop_region)
+            crop_image_id = self.get_crop_image_id(entry['image_id'], crop_region)
             crop_img = self.crop_image(img, crop_region)
             variance = self.compute_variance(crop_img)
-            print(f'Computed variance {variance} for {crop_path}')
-            self.save_crop(crop_img, variance)
+
+            new_entry = self.create_entry(entry, crop_image_id, variance, crop_region)
+            self.result.append(new_entry)
+
+            dest_crop_path = self.get_dest_crop_path(filename, crop_image_id)
+            self.save_crop(crop_img, dest_crop_path)
+            print(f'Saved crop {dest_crop_path} with variance {variance}')
 
 
     def compute_crop_regions(self, img):
@@ -86,16 +101,27 @@ class CropWithVariance:
         return crops
 
 
-    def get_crop_path(self, filename, crop_region):
+    def get_crop_image_id(self, image_id, crop_region):
         x, y = crop_region
 
         pattern = re.compile(r'(.+)(?:\(.*\))')
-        image_prefix = re.match(pattern, filename).group(1)
-        image_ext = os.path.splitext(filename)[1]
+        image_prefix = re.match(pattern, image_id).group(1)
+        image_ext = os.path.splitext(image_id)[1]
         coords = f"({x},{y},{x + self.crop_size},{y + self.crop_size})"
-        new_image_path = f'{image_prefix}{coords}{image_ext}'
+        new_image_id = f'{image_prefix}{coords}{image_ext}'
 
-        return new_image_path
+        return new_image_id
+
+
+    def get_dest_crop_path(self, filename, crop_image_id):
+        # Determine the destination filename
+        relative_path = os.path.relpath(filename, self.origin_dir)
+        dest_filename = os.path.join(self.dest_dir, relative_path)
+
+        dest_dir = os.path.dirname(dest_filename)
+        dest_crop_path = os.path.join(dest_dir, crop_image_id)
+
+        return dest_crop_path
 
 
 
@@ -117,17 +143,31 @@ class CropWithVariance:
         return mean_variance
 
 
+    def create_entry(self, entry, crop_image_id, variance, crop_region):
+        x, y = crop_region
+        new_entry = entry.copy()
+        new_entry['image_id'] = crop_image_id
+        new_entry['variance'] = variance
+        new_entry['x'] = x
+        new_entry['y'] = y
+        new_entry['w'] = self.crop_size
+        new_entry['h'] = self.crop_size
+        return new_entry
+
+
     def save_crop(self, crop_img, filename):
-        pass
+        dest_dir = os.path.dirname(filename)
+        os.makedirs(dest_dir, exist_ok=True)
+        crop_img.save(filename)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Crop images and save variance')
+    parser.add_argument('csv_images', help='CSV file containing image filenames')
     parser.add_argument('--dest_dir', help='Directory to save cropped images')
     parser.add_argument('--crop_size', type=int, help='Size of crop')
     parser.add_argument('--shift', type=int, help='Shift of crop')
-    parser.add_argument('--csv_images', help='CSV file containing image filenames')
     args = parser.parse_args()
 
     crop_with_variance = CropWithVariance(args.csv_images, args.dest_dir, args.crop_size, args.shift)
-    crop_with_variance.crop_and_discard(args.csv_images)
+    crop_with_variance.crop_and_discard()
