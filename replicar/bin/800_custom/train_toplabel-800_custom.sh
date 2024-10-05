a5e003b64a6002a08d095c3b2fe060072d13f089
Joan Puigcerver <joapuiib@gmail.com>
2024-10-05 20:55:25 +0200

    script 800 toplabel

---
+++ replicar/bin/800_custom/train_toplabel-800_custom.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+target="top_label"
+size="800_custom"
+epochs=50
+batch_size=20
+n_workers=8
+lr="1e-4"
+weight_decay="1e-5"
+variance=150
+
+model_dir="$HOME/tfm/replicar/models/$target-$size"
+model_path="$model_dir/model.pt"
+
+replicar="$HOME/tfm/replicar/train.py \
+    --target $target \
+    --size $size \
+    --batch_size $batch_size \
+    --n_workers $n_workers \
+    --model_path $model_path \
+    --epochs $epochs \
+    --lr $lr \
+    --weight_decay $weight_decay \
+    --variance_threshold $variance \
+    $@"
+datetime=$(date +%Y%m%d-%H%M%S)
+
+mkdir -p $model_dir
+echo $replicar > $model_dir/${datetime}_stdout.log
+($replicar | tee -a $model_dir/${datetime}_stdout.log) 3>&1 1>&2 2>&3 | tee $model_dir/${datetime}_stderr.log
