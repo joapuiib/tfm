2f25f772aa24a782721fd4bf4744456cd4cdee3f
Joan Puigcerver <joapuiib@gmail.com>
2024-10-15 16:32:51 +0200

    updated script to use venv

--- replicar/bin/800_custom/train_toplabel-800_custom.sh
+++ replicar/bin/800_custom/train_toplabel-800_custom.sh
@@ -11,7 +11,8 @@ variance=150
 model_dir="$HOME/tfm/replicar/models/$target-$size"
 model_path="$model_dir/model.pt"
 
-replicar="$HOME/tfm/replicar/train.py \
+replicar="python
+    $HOME/tfm/replicar/train.py \
     --target $target \
     --size $size \
     --batch_size $batch_size \
@@ -23,6 +24,8 @@ replicar="$HOME/tfm/replicar/train.py \
     --variance_threshold $variance \
     $@"
 datetime=$(date +%Y%m%d-%H%M%S)
+
+source $HOME/tfm/.venv/bin/activate
 
 mkdir -p $model_dir
 echo $replicar > $model_dir/${datetime}_stdout.log
