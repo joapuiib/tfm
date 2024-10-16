32b1d8b2e1d0ce3ea11ffd2c5ad6ad71eb5d1607
Joan Puigcerver <joapuiib@gmail.com>
2024-10-16 22:39:26 +0200

    entrenament toplabel 800custom

---
+++ replicar/bin/800_custom/test_toplabel-800_custom_against_800.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+target="top_label"
+size="800"
+epochs=50
+batch_size=20
+n_workers=8
+lr="1e-4"
+weight_decay="1e-5"
+
+model_dir="$HOME/tfm/replicar/models/$target-${size}_custom"
+model_path="$model_dir/model.pt"
+
+replicar="$HOME/tfm/replicar/train.py \
+    --test \
+    --target $target \
+    --size $size \
+    --batch_size $batch_size \
+    --n_workers $n_workers \
+    --model_path $model_path \
+    --epochs $epochs \
+    --lr $lr \
+    --weight_decay $weight_decay \
+    $@"
+datetime=$(date +%Y%m%d-%H%M%S)
+
+source $HOME/tfm/.venv/bin/activate
+
+mkdir -p $model_dir
+echo $replicar > $model_dir/${datetime}_stdout.log
+($replicar | tee $model_dir/${datetime}_test_stdout.log) 3>&1 1>&2 2>&3 | tee $model_dir/${datetime}_test_stderr.log
